7007959c6bbd8a5357bf656e9e615be602391a0f
Ayush Shrivastava <ayush@Ayushs-MacBook-Air.local>
2026-02-14 17:11:13 +0530

    03_Home_Navbar_RBA

--- modules/auth/actions/index.js
+++ modules/auth/actions/index.js
@@ -1,45 +1,70 @@
-"use server"
-import { db } from "@/lib/db"
-import { currentUser } from "@clerk/nextjs/server"
+"use server";
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { Select } from "react-day-picker";
 
-
-export const onBoardUser = async()=>{
-   try {
+export const onBoardUser = async () => {
+  try {
     const user = await currentUser();
-    if(!user){
-        return {success: false, error:"No authenticated user found"};
+    if (!user) {
+      return { success: false, error: "No authenticated user found" };
     }
 
-    const {id, firstName, lastName, imageUrl, emailAddresses} = user;
+    const { id, firstName, lastName, imageUrl, emailAddresses } = user;
 
     const newUser = await db.user.upsert({
-        where:{
-            clerkId:id
-        },
-        update:{
-           firstName: firstName || null,
-           lastName: lastName || null,
-           imageUrl: imageUrl || null,
-           email: emailAddresses[0]?.emailAddress || "", 
-        },
-        create:{
-           clerkId: id,
-           firstName: firstName || null,
-           lastName: lastName || null,
-           imageUrl: imageUrl || null,
-           email: emailAddresses[0]?.emailAddress || "", 
-        }
+      where: {
+        clerkId: id,
+      },
+      update: {
+        firstName: firstName || null,
+        lastName: lastName || null,
+        imageUrl: imageUrl || null,
+        email: emailAddresses[0]?.emailAddress || "",
+      },
+      create: {
+        clerkId: id,
+        firstName: firstName || null,
+        lastName: lastName || null,
+        imageUrl: imageUrl || null,
+        email: emailAddresses[0]?.emailAddress || "",
+      },
     });
     return {
-        success: true,
-        user: newUser,
-        messge: "User onboarded successuly"
+      success: true,
+      user: newUser,
+      messge: "User onboarded successuly",
+    };
+  } catch (error) {
+    console.log("❌ Error onboarding user:", error);
+    return {
+      success: false,
+      error: "Failed to Onboarded user",
+    };
+  }
+};
+
+export const currentUserRole = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return { success: false, error: "No authenticated user" };
     }
-   } catch (error) {
-    console.log("❌ Error onboarding user:",error);
-    return{
-        success: false,
-        error: "Failed to Onboarded user"
-    }
-   }
-}; 
+    const { id } = user;
+    const userRole = await db.user.findUnique({
+      where: {
+        clerkId: id,
+      },
+      select: {
+        role: true,
+      },
+    });
+    return userRole.role;
+  } catch (error) {
+    console.log("❌ Error onboarding user:", error);
+    return {
+      success: false,
+      error: "Failed to Onboarded user",
+    };
+  }
+};
