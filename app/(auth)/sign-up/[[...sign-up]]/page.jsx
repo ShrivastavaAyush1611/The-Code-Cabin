8626b5899b371e42d613481329e8d9a42d361a7c
Ayush Shrivastava <ayush@Ayushs-MacBook-Air.local>
2026-02-14 15:13:32 +0530

    02 user OnBoarding

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import { SignUp } from '@clerk/nextjs'
+import { mainModule } from 'node:process'
+
+function SignUpPage() {
+  return (
+    
+      <SignUp/>
+    
+  )
+}
+
+export default SignUpPage
